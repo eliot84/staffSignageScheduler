60cb880bf85d619ccb038c8b2d34e931c4820e43
Eliot Rodriguez <mr.ear84@gmail.com>
2019-06-17 11:50:31 -0400

    adding moment.js for time tracking

--- assets/js/staffSignage.js
+++ assets/js/staffSignage.js
@@ -6,8 +6,10 @@ console.log("staffSignage.js is online");
 
 //example google sheet get: "https://sheets.googleapis.com/v4/spreadsheets/spreadsheetId/values/Sheet1!A1:D5";
 
+console.log(moment().format('MMMM Do YYYY, h:mm:ss a') );
+
 const Http = new XMLHttpRequest();
-const url = "https://sheets.googleapis.com/v4/spreadsheets/1rf3Ni0TRr6paXqN-kX33L292RhX1vTPBbw4diIOL6EY/values/Sheet1!A1:D5?key=AIzaSyApWcCYigYzktDJE3WY5JImjthvjN3X5jA";
+const url = "https://sheets.googleapis.com/v4/spreadsheets/1rf3Ni0TRr6paXqN-kX33L292RhX1vTPBbw4diIOL6EY?ranges=Sheet1&key={302308272083-ve340842p2h65jhmosgm2qatigsu2rsi.apps.googleusercontent.com}";
 Http.open("GET", url);
 Http.send();
 
