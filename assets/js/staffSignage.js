e0fa39388d029d16b16b4995d6524ad8b2b5a4b7
Eliot Rodriguez <mr.ear84@gmail.com>
2019-06-17 13:02:53 -0400

    fixed moment js problems and now displaying on page

--- assets/js/staffSignage.js
+++ assets/js/staffSignage.js
@@ -6,8 +6,18 @@ console.log("staffSignage.js is online");
 
 //example google sheet get: "https://sheets.googleapis.com/v4/spreadsheets/spreadsheetId/values/Sheet1!A1:D5";
 
-console.log(moment().format('MMMM Do YYYY, h:mm:ss a') );
+const clock = document.getElementById('clock');
 
+function updateTime(){
+	const now = moment();
+	const humanReadable = now.format('dddd') + ' ' + now.format('MMMM Do YYYY, h:mm:ss a');
+
+	clock.textContent = humanReadable;
+}
+
+setInterval(updateTime, 1000);
+
+/*
 const Http = new XMLHttpRequest();
 const url = "https://sheets.googleapis.com/v4/spreadsheets/1rf3Ni0TRr6paXqN-kX33L292RhX1vTPBbw4diIOL6EY?ranges=Sheet1&key={302308272083-ve340842p2h65jhmosgm2qatigsu2rsi.apps.googleusercontent.com}";
 Http.open("GET", url);
@@ -17,4 +27,4 @@ Http.onreadystatechange=(e)=>{
 	console.log(Http.responseText)
 }
 
-
+*/
