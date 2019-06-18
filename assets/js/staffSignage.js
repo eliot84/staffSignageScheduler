e4e9eff086f1925d7df88f0ce7602eacf9850115
Eliot Rodriguez <mr.ear84@gmail.com>
2019-06-18 09:46:01 -0400

    test again

--- assets/js/staffSignage.js
+++ assets/js/staffSignage.js
@@ -13,7 +13,7 @@ setInterval(updateTime, 1000);
 
 
 const Http = new XMLHttpRequest();
-const url = "https://sheets.googleapis.com/v4/spreadsheets/1GnI7OZ-uzE4qzCQh8zsHNFi9yA9z6_z7KMUg14yZHYg?ranges=Sheet1&fields=sheets%2Fdata%2FrowData%2CspreadsheetUrl&key=dsG2vpOLB5vO52ogkkBuOd83";
+const url = "https://sheets.googleapis.com/v4/spreadsheets/1GnI7OZ-uzE4qzCQh8zsHNFi9yA9z6_z7KMUg14yZHYg?ranges=Sheet1&fields=sheets%2Fdata%2FrowData%2CspreadsheetUrl&key=AIzaSyAQQWeauYhpITuv3oyMfBenmj27TI7OOtA";
 Http.open("GET", url);
 Http.send();                                                                                                                                                                                                                      
 
