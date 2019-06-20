20200f9f1994a11a49044ab5000fabb062c9e89a
Eliot Rodriguez <mr.ear84@gmail.com>
2019-06-20 09:48:14 -0400

    pulling data from googleSheet API and displaying it on webpage

--- assets/js/staffSignage.js
+++ assets/js/staffSignage.js
@@ -1,6 +1,7 @@
 console.log("staffSignage.js is online");
 
 const clock = document.getElementById('clock');
+var result;
 
 function updateTime(){
 	const now = moment();
@@ -13,12 +14,28 @@ setInterval(updateTime, 1000);
 
 
 const Http = new XMLHttpRequest();
-const url = "https://sheets.googleapis.com/v4/spreadsheets/1GnI7OZ-uzE4qzCQh8zsHNFi9yA9z6_z7KMUg14yZHYg?ranges=Sheet1&fields=sheets%2Fdata%2FrowData%2CspreadsheetUrl&key=AIzaSyAQQWeauYhpITuv3oyMfBenmj27TI7OOtA";
+const url = "https://sheets.googleapis.com/v4/spreadsheets/1GnI7OZ-uzE4qzCQh8zsHNFi9yA9z6_z7KMUg14yZHYg/values/A1%3AB12?valueRenderOption=FORMATTED_VALUE&key=AIzaSyAQQWeauYhpITuv3oyMfBenmj27TI7OOtA"
+;
+
 Http.open("GET", url);
 Http.send();                                                                                                                                                                                                                      
 
 Http.onreadystatechange=(e)=>{
-	console.log(Http.responseText)
+	result = JSON.parse(Http.responseText);
+	populate(result);
+
+}
+
+
+function populate(result){
+	var staff = result.values;
+	var findList = document.getElementById('list');
+
+	for(i = 1; i < staff.length; i++){
+		var addThis = '<div class="card"><a class="portLinks" href="portfolio/BTTFHangman.html"><div class="cardPhoto a"></div><h1>' + staff[i][0] + '</h1> <p>Service Desk Technician</p></a> </div>';
+
+		findList.insertAdjacentHTML("beforeend", addThis);
+	}		
 }
 
 
